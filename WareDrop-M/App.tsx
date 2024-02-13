35c19442cea287230fcf4fbd429b4b8b7bb700ec
Darkkhalys <145651320+Darkkhalys@users.noreply.github.com>
2024-02-13 12:40:51 +0100

    Add files via upload

---
+++ WareDrop-M/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>MobilApp fálj helye</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
