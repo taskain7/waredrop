af079868812fcd2cc9b8309dfa2b209336f00917
Darkkhalys <145651320+Darkkhalys@users.noreply.github.com>
2024-02-16 08:54:31 +0100

    Update App.tsx

--- WareDrop-M/App.tsx
+++ WareDrop-M/App.tsx
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>MobilApp fálj helye</Text>
       <StatusBar style="auto" />
     </View>
   );
