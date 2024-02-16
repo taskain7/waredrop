7d1881d016ad7febe4742b1b8478f536965e0e4b
R4P7ORR <peetya.77@gmail.com>
2024-02-16 08:51:03 +0100

    Attended to requested changes for pull request

--- WareDrop_Frontend/waredrop_frontend/src/index.tsx
+++ WareDrop_Frontend/waredrop_frontend/src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom/client';import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
